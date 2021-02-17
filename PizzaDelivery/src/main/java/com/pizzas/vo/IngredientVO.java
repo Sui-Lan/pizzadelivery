cc5c051b1749f894dbe568d470fb872c933b0ead
Sui <sgonzalezmac@gmail.com>
2021-02-17 01:11:25 -0300

    Creación objeto VO

--- PizzaDelivery/src/main/java/com/pizzas/vo/IngredientVO.java
+++ PizzaDelivery/src/main/java/com/pizzas/vo/IngredientVO.java
@@ -1,9 +1,21 @@
 package com.pizzas.vo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.pizzas.model.Ingredient;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class IngredientVO {
 
     private String message;
