0edbc857342857d3cdffb850b36f402a3c28f084
Sui <sgonzalezmac@gmail.com>
2021-02-17 01:11:54 -0300

    Creación del DAO

--- PizzaDelivery/src/main/java/com/pizzas/dao/IIngredientRepository.java
+++ PizzaDelivery/src/main/java/com/pizzas/dao/IIngredientRepository.java
@@ -1,10 +1,17 @@
 package com.pizzas.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.pizzas.model.Ingredient;
+import com.pizzas.vo.IngredientVO;
 
+@Repository
 public interface IIngredientRepository extends JpaRepository<Ingredient, Integer> {
 
-    
+    @Query(value="SELECT * FROM ingredient WHERE name = ?1", nativeQuery=true)
+    public List<Ingredient> findByName(String name);
 }
