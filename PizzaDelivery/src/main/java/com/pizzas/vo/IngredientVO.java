059a8c717e7a3d810a94d606baab6eeecdd0d32b
Sui <sgonzalezmac@gmail.com>
2021-02-16 22:35:40 -0300

    Creación del objeto VO de Ingredient

---
+++ PizzaDelivery/src/main/java/com/pizzas/vo/IngredientVO.java
@@ -0,0 +1,13 @@
+package com.pizzas.vo;
+
+import java.util.List;
+
+import com.pizzas.model.Ingredient;
+
+public class IngredientVO {
+
+    private String message;
+    private String code;
+    private List<Ingredient> IngredientList;
+    
+}
