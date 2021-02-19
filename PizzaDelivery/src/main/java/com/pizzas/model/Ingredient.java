afe23cf506189b72c9938765f6068cc8df1b7126
Sui <sgonzalezmac@gmail.com>
2021-02-19 00:12:14 -0300

    Corrección a Enum de la clase Ingredient

--- PizzaDelivery/src/main/java/com/pizzas/model/Ingredient.java
+++ PizzaDelivery/src/main/java/com/pizzas/model/Ingredient.java
@@ -1,24 +1,34 @@
 package com.pizzas.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Ingredient {
     
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer idIngredient;
     private String nameIngredient;
-    private float stockQuantity;
-    private Enum measureUnit;
+    private Float stockQuantity;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "measure_unit", columnDefinition="ENUM('KILOS','LITROS')", nullable = false)
+    private MeasurementUnit measurementUnit;
     
 }
