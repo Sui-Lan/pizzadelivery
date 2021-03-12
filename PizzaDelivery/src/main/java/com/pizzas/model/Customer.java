2fc0e3beb412acfeb2dc5e3ac662eefcd8717619
Sui <sgonzalezmac@gmail.com>
2021-03-12 02:01:15 -0300

    Creación de Customer entity

---
+++ PizzaDelivery/src/main/java/com/pizzas/model/Customer.java
@@ -0,0 +1,31 @@
+package com.pizzas.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idCustomer;
+    private String nameCustomer;
+    private String lastnameCustomer;
+    private String email;
+    private int phone;
+    
+}
