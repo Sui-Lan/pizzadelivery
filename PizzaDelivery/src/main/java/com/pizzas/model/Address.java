42ffcc3a4aadefc6678869a6e03e48d8c5742fe0
Sui <sgonzalezmac@gmail.com>
2021-03-12 02:00:34 -0300

    Correcion de asociación

--- PizzaDelivery/src/main/java/com/pizzas/model/Address.java
+++ PizzaDelivery/src/main/java/com/pizzas/model/Address.java
@@ -4,6 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,6 +26,10 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idAddress;
+    @ManyToOne
+    @MapsId("idCustomer")
+    @JoinColumn(name = "id_customer")
+    private Customer customer;
     private String nameAddress;
     private String street;
     private String number;
